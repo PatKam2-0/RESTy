b102dbe69b30fef2ff649fa59d8df29f63b99ead
Pat Haynes <pathaynes32@gmail.com>
2019-11-01 14:18:54 -0700

    set up header and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Footer.css'; 
+
+
+const Footer = () => (
+  <footer>
+    <h2 className={styles}>©2019 Alchemy Code Lab</h2>
+  </footer>
+);
+
+export default Footer;
